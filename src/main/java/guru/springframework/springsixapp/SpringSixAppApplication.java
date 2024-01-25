ab3fada863963f48d2076568bfb937dac456394c
Alex <kardulaalex@gmail.com>
2024-01-25 15:14:50 +0100

    Just JPA Entities and Relations

---
+++ src/main/java/guru/springframework/springsixapp/SpringSixAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springsixapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSixAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSixAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springsixapp/SpringSixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springsixapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSixAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
